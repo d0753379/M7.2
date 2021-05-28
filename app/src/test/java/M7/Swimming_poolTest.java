cf6d72a786ab8e875aea6ee8003953dee4cd4eb7
d0753379 <d0753379@mail.fcu.edu.tw>
2021-05-28 17:20:58 +0800

    Update Swimming_poolTest.java

--- app/src/test/java/M7/Swimming_poolTest.java
+++ app/src/test/java/M7/Swimming_poolTest.java
@@ -1,4 +1,4 @@
-package M6_2;
+﻿package M6_2;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -104,12 +104,14 @@ class Swimming_poolTest {
 		x.countPrice();
 		assertEquals(160, x.price);
 	}
+	/*
 	@Test
 	void test11() throws Exception {
 		Swimming_pool x = new Swimming_pool(false, false, false, 8, 0, 3);
 		x.countPrice();
 		assertEquals(160, x.price);
 	}
+	*/
 
 	
 }
