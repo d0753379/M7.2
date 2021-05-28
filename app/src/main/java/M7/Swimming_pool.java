08d986c6181543788f1d5a57f105e8dd34423714
d0753379 <d0753379@mail.fcu.edu.tw>
2021-05-28 15:41:16 +0800

    Initial commit

---
+++ app/src/main/java/M7/Swimming_pool.java
@@ -0,0 +1,72 @@
+package M6_2;
+
+public class Swimming_pool {
+	int price;
+	boolean holiday;
+	boolean member;
+	boolean group;
+	int hour;
+	int min;
+	int age;
+
+	public Swimming_pool(boolean holiday, boolean member, boolean group, int hour, int min, int age) {
+		this.price = -1;
+		this.holiday = holiday;
+		this.member = member;
+		this.group = group;
+		this.hour = hour;
+		this.min = min;
+		this.age = age;
+	}
+
+	public void countPrice() throws Exception {
+		if (check()) {
+		} else {
+			throw new Exception("Error");
+		}
+
+		if (holiday) {
+			if (member == true) {
+				price = 125;
+			} else {
+				price = 250;
+			}
+		} else {
+			if (member == true) {
+				price = 100;
+			} else {
+				if (group == true) {
+					price = 140;
+				} else {
+					if (countTime() == true || countAge() == true) {
+						price = 160;
+					} else {
+						price = 200;
+					}
+				}
+			}
+		}
+	}
+
+	boolean check() {
+		if (age < 3 || age > 75) {
+			return false;
+		}
+		return true;
+	}
+
+	boolean countTime() {
+		if (hour < 7 || hour == 7 && min == 0) {
+			return true;
+		}
+
+		return false;
+	}
+
+	boolean countAge() {
+		if (age <= 12 || 60 <= age) {
+			return true;
+		}
+		return false;
+	}
+}
---
+++ app/src/test/java/M7/Swimming_poolTest.java
@@ -0,0 +1,115 @@
+package M6_2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class Swimming_poolTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test1() {
+		Swimming_pool x = new Swimming_pool(true, false, false, 22, 1, 2);
+		Exception exception = assertThrows(Exception.class,
+				() -> x.countPrice());
+		assertEquals("Error", exception.getMessage());
+	}
+
+	@Test
+	void test2() throws Exception {
+		Swimming_pool x = new Swimming_pool(true, false, false, 7, 1, 13);
+		x.countPrice();
+		
+
+		assertEquals(250, x.price);
+	}
+
+	@Test
+	void test3() throws Exception {
+		Swimming_pool x = new Swimming_pool(false, false, false, 7, 1, 13);
+		x.countPrice();
+
+		assertEquals(200, x.price);
+	}
+
+	@Test
+	void test4() throws Exception {
+		Swimming_pool x = new Swimming_pool(false, false, false, 5, 0, 3);
+		x.countPrice();
+
+		assertEquals(160, x.price);
+	}
+
+	@Test
+	void test5() throws Exception {
+		Swimming_pool x = new Swimming_pool(false, false, true, 5, 0, 60);
+		x.countPrice();
+
+		assertEquals(140, x.price);
+	}
+
+	@Test
+	void test6() throws Exception {
+		Swimming_pool x = new Swimming_pool(true, true, true, 5, 0, 3);
+		x.countPrice();
+
+		assertEquals(125, x.price);
+	}
+
+	@Test
+	void test7() throws Exception {
+		Swimming_pool x = new Swimming_pool(false, true, true, 5, 0, 3);
+		x.countPrice();
+
+		assertEquals(100, x.price);
+	}
+
+	@Test
+	void test8() {
+		Swimming_pool x = new Swimming_pool(true, false, false, 7, 1, 76);
+		Exception exception = assertThrows(Exception.class,
+				() -> x.countPrice());
+		assertEquals("Error", exception.getMessage());
+	}
+
+	// 補涵蓋度
+	@Test
+	void test9() throws Exception {
+		Swimming_pool x = new Swimming_pool(false, false, false, 7, 0, 60);
+		x.countPrice();
+		assertEquals(160, x.price);
+	}
+	@Test
+	void test10() throws Exception {
+		Swimming_pool x = new Swimming_pool(false, false, false, 8, 0, 60);
+		x.countPrice();
+		assertEquals(160, x.price);
+	}
+	@Test
+	void test11() throws Exception {
+		Swimming_pool x = new Swimming_pool(false, false, false, 8, 0, 3);
+		x.countPrice();
+		assertEquals(160, x.price);
+	}
+
+	
+}
